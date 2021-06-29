bd62d52011e63fa9b30697446382f814464c61f2
Viz0616 <josevizcaya0616@gmail.com>
2021-06-29 16:15:17 -0400

    MID-#0008 token verif. implmntd

--- app/users/resources/subscribe_rest.py
+++ app/users/resources/subscribe_rest.py
@@ -1,4 +1,5 @@
 from flask_restful import Resource, reqparse, marshal_with, abort
+from flask_jwt_extended import jwt_required, current_user
 from ...schemas import UserSchema
 from ...models import User, Channel
 from ... import db
@@ -9,46 +10,52 @@ from ...common.status_handlers import Success
 
 class SubscribeREST(Resource):
     @marshal_with(SUCCESS_DICT)
-    def put(self, channel_id):
+    @jwt_required()
+    def post(self):
         args = self.create_args()
 
-        user = User.query.filter_by(username=args['username']).first()
-        channel = Channel.query.filter_by(id=channel_id).first()
+        if args['channelName']:
+            channel = Channel.get_channel_name(args['channelName'])
+        if args['channelId']:
+            channel = Channel.get_channel_id(args['channelId'])
 
-        if not user or not channel:
+        if not channel:
             abort(404, message="Channel or User does not exist...")
-        if channel in user.subcribed_to:
+        if channel in current_user.subcribed_to:
             abort(
-                409, message=f'{user.username} is already subscribed to {channel.channel_name}')
+                409, message=f'{current_user.username} is already subscribed to {channel.channel_name}')
 
-        user.subcribed_to.append(channel)
-        db.session.add(user)
+        current_user.subcribed_to.append(channel)
+        db.session.add(current_user)
         db.session.commit()
 
-        return Success(data=f'{args["username"]} > {channel.channel_name}', message="User successfully subscribedl", from_request='PUT', status_code=200)
+        return Success(data=f'{current_user} > {channel.channel_name}', message="User successfully subscribedl", from_request='POST', status_code=201), 201
 
     @marshal_with(SUCCESS_DICT)
-    def delete(self, channel_id):
+    @jwt_required()
+    def delete(self):
         args = self.create_args()
 
-        user = User.query.filter_by(username=args['username']).first()
-        channel = Channel.query.filter_by(id=channel_id).first()
+        if args['channelName']:
+            channel = Channel.get_channel_name(args['channelName'])
+        if args['channelId']:
+            channel = Channel.get_channel_id(args['channelId'])
 
-        if not user or not channel:
+        if not channel:
             abort(404, message="Channel or User does not exist...")
-        if not channel in user.subcribed_to:
+        if not channel in current_user.subcribed_to:
             abort(
-                409, message=f"{user.username} is already desubscribed to {channel.channel_name}")
+                409, message=f"{current_user.username} is already desubscribed to {channel.channel_name}")
 
-        user.subcribed_to.remove(channel)
-        db.session.add(user)
+        current_user.subcribed_to.remove(channel)
+        db.session.add(current_user)
         db.session.commit()
 
-        return Success(data=f'{args["username"]} x {channel.channel_name}', message="User successfully desubscribedl", from_request='PUT', status_code=200)
+        return Success(data=f'{current_user} not {channel.channel_name}', message="User successfully desubscribedl", from_request='DELETE', status_code=200)
 
     def create_args(self):
         post_args = reqparse.RequestParser()
-        post_args.add_argument(
-            "username", help="The USERNAME is required", required=True)
+        post_args.add_argument("channelName")
+        post_args.add_argument("channelId")
 
         return post_args.parse_args()
