fcf682713ed462ca996cbd030fcaa8f80e3e1803
Viz0616 <josevizcaya0616@gmail.com>
2021-06-29 16:00:59 -0400

    MID-#0003 JTW def. and implement.

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,26 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from .common.config import Config
+
 
 db = SQLAlchemy()
 ma = Marshmallow()
 cors = CORS()
+jwt = JWTManager()
+
+
+@jwt.user_identity_loader
+def user_identity_lookup(user):
+    return user.id
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    from .models import User
+    identity = jwt_data['sub']
+    return User.get_user_id(identity)
 
 
 def create_app(config=Config):
@@ -16,6 +31,7 @@ def create_app(config=Config):
     db.init_app(app)
     ma.init_app(app)
     cors.init_app(app)
+    jwt.init_app(app)
 
     from .users.routes import users_bp
     from .videos.routes import videos_bp
