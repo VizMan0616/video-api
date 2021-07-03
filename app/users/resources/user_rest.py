cb955919be2339e5fed4bf94de0fb3a1f0242ec3
Viz0616 <josevizcaya0616@gmail.com>
2021-07-03 17:56:56 -0400

    MID-#0017 jwt token verification added to several routes

--- app/users/resources/user_rest.py
+++ app/users/resources/user_rest.py
@@ -6,6 +6,7 @@ from ...models import User, Channel
 from ... import db
 from ...common.constants import SUCCESS_DICT
 from ...common.status_handlers import Success
+from ...common.utils import create_user_folder
 
 
 class UserREST(Resource):
@@ -19,62 +20,54 @@ class UserREST(Resource):
 
         user = User(args['username'], args['firstName'], args['lastName'], dt.strptime(
             args['birthDate'], '%Y-%m-%dT%H:%M:%S.%fZ'), args['sexId'], args['email'], args['password'])
+        create_user_folder(user.username)
         db.session.add(user)
         db.session.commit()
 
         channel = Channel(user.username, user.id)
         db.session.add(channel)
         db.session.commit()
-
         return Success(data=user.username, message="User added successfully", status_code=201, from_request="POST"), 201
 
     @marshal_with(SUCCESS_DICT)
     @jwt_required()
-    def put(self, user_id):
+    def put(self):
         args = self.create_optional_args()
 
         current_user.username = args['username'] or current_user.username
-        current_user.name = args['firstName'] or current_user.name
+        current_user.first_name = args['firstName'] or current_user.first_name
         current_user.last_name = args['lastName'] or current_user.last_name
 
-        if args['birthDate']:
-            current_user.birth_date = datetime.strptime(
-                args['birthDate'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        current_user.user_channel.channel_name = args['username'] or current_user.username
 
-        user.sex_id = args['sexId'] or current_user.sex_id
-
-        user.user_channel.channel_name = args['username'] or user.username
-
-        db.session.add(user)
+        db.session.add(current_user)
         db.session.commit()
 
-        return Success(data=user.username, message="User updated successfully", status_code=200, from_request="PUT"), 200
+        return Success(data=current_user.username, message="User updated successfully", status_code=200, from_request="PUT")
 
     def create_args(self):
         post_args = reqparse.RequestParser()
         post_args.add_argument(
-            "username", help="The USERNAME is required", required=True)
+            "username", help="The USERNAME is required", type=str, required=True)
         post_args.add_argument(
-            "firstName", help="The FIRST NAME is required", required=True)
+            "firstName", help="The FIRST NAME is required", type=str, required=True)
         post_args.add_argument(
-            "lastName", help="The LAST NAME is required", required=True)
+            "lastName", help="The LAST NAME is required", type=str, required=True)
         post_args.add_argument(
-            "birthDate", help="The BIRTH DATE is required", required=True)
+            "birthDate", help="The BIRTH DATE is required", type=str, required=True)
         post_args.add_argument(
-            "sexId", help="The SEX ID is required", required=True)
+            "sexId", help="The SEX ID is required", type=int, required=True)
         post_args.add_argument(
-            "email", help="The EMAIL is required", required=True)
+            "email", help="The EMAIL is required", type=str, required=True)
         post_args.add_argument(
-            "password", help="The PASSWORD is required", required=True)
+            "password", help="The PASSWORD is required", type=str, required=True)
 
         return post_args.parse_args()
 
     def create_optional_args(self):
         put_args = reqparse.RequestParser()
-        put_args.add_argument("username")
-        put_args.add_argument("firstName")
-        put_args.add_argument("lastName")
-        put_args.add_argument("birthDate")
-        put_args.add_argument("sexId")
+        put_args.add_argument("username", type=str)
+        put_args.add_argument("firstName", type=str)
+        put_args.add_argument("lastName", type=str)
 
         return put_args.parse_args()
