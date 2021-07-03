9bf5c37ab678fc71244c4a5691e4c8a46beda7c5
Viz0616 <josevizcaya0616@gmail.com>
2021-07-03 18:04:56 -0400

    MID-#0021 Added handle random videos request

--- app/videos/resources/videos_rest.py
+++ app/videos/resources/videos_rest.py
@@ -1,39 +1,12 @@
-from flask_restful import Resource, reqparse, abort, marshal_with
-from ... import db
-from ...models import Video, Details
+from flask_restful import Resource
+from ...models import Video
 from ...schemas import VideoSchema
-from ...common.constants import SUCCESS_DICT
-from ...common.status_handlers import Success
 
 
 class VideosREST(Resource):
     def get(self):
-        return VideoSchema(many=True).dump(Video.get_videos())
+        return VideoSchema(many=True).dump(Video.get_random_videos_order())
 
-    @marshal_with(SUCCESS_DICT)
-    def post(self):
-        args = self.create_args()
 
-        video = Video(args['videoTitle'], args['channelId'],
-                      args['description'] or '')
-        db.session.add(video)
-        db.session.commit()
-
-        details = Details(video.id)
-        db.session.add(details)
-        db.session.commit()
-
-        return Success(data=video.video_title, message="Video successfully uploaded", status_code=201)
-
-    def create_args(self):
-        post_parse = reqparse.RequestParser()
-
-        post_parse.add_argument(
-            'videoTitle', help='The TITLE is required', required=True)
-        post_parse.add_argument('description')
-        post_parse.add_argument(
-            'channelId', help='The CHANNEL ID is required', required=True)
-
-        return post_parse.parse_args()
 
 
